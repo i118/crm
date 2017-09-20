45b2fb4c7a6a435a6196b428d1f6af846ca972fc
Ghostik2005 <sergobel78@gmail.com>
2017-09-20 14:54:35 +0300

    ustable edition

---
+++ html/libs/skin.js
@@ -0,0 +1,3 @@
+//[Skin Customization]
+webix.skin.material.barHeight=45;webix.skin.material.tabbarHeight=36;webix.skin.material.rowHeight=34;webix.skin.material.listItemHeight=34;webix.skin.material.inputHeight=29;webix.skin.material.layoutMargin.wide=4;webix.skin.material.layoutMargin.space=10;webix.skin.material.layoutPadding.space=10;
+ webix.skin.set('material')
